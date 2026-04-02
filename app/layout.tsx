326beaff54181aa33ae1a6e543c88f5ad331abf1
Dharmesh <dharmeshwr@gmail.com>
2026-04-02 16:51:36 +0530

    fix: favicon issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,11 +59,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico", type: "image/x-icon" },
-      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-      { url: "/favicon.svg", type: "image/svg+xml" },
     ],
     apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      { url: "/apple-icon.png", sizes: "180x180", type: "image/png" },
     ],
     shortcut: ["/favicon.ico"],
   },
